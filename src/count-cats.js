5dd3411de4cee84da0395d880aa1aa100d702571
Nastishka <anastasiya.matronkina.com>
2020-03-06 00:17:01 +0300

    feat: implement countCats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+const EARS = '^^';
+module.exports = function countCats(matrix) {
+  if (Array.isArray(matrix) && matrix.length > 0) {
+    return matrix
+      .flat(Infinity)
+      .filter(element => element === EARS)
+      .length;
+  }
+  return 0;
 };
