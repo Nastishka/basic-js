57a3d16343245a5c7a3e247afa58a853683c5107
Nastishka <anastasiya.matronkina.com>
2020-03-21 23:10:57 +0300

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,19 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+const repeatStr = (str, repeatTimes, separator) => {
+    let result = str;
+    if (repeatTimes > 0) {
+        let resultArray = Array.from({ length: repeatTimes }, () => result);
+        result = resultArray.join(separator);
+    }
+    return result;
+}
+module.exports = function repeater(str, options) {
+    let addition = repeatStr(
+        options.addition === undefined ? '' : options.addition + '',
+        options.additionRepeatTimes,
+        options.additionSeparator || '|');
+    
+    return repeatStr(
+        addition ? str + addition : str,
+        options.repeatTimes,
+        options.separator || '+');
 };
-  
