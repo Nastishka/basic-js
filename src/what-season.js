ec011578edde28ac26cb8839287629e9c5914956
Nastishka <anastasiya.matronkina.com>
2020-03-22 00:02:36 +0300

    feat: implement getSeason  function

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,34 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date) {
+    if (Object.prototype.toString.call(date) === '[object Date]') {
+      let month = date.getMonth() + 1;
+      let result = '';
+      switch (month) {
+        case 1:
+        case 2:
+        case 12:
+          result = 'winter';
+          break;
+        case 3:
+        case 4:
+        case 5:
+          result = 'spring';
+          break;
+        case 6:
+        case 7:
+        case 8:
+            result = 'summer';
+            break;
+        case 9:
+        case 10:
+        case 11:
+            result = 'autumn';
+        break;
+      }
+      return result;
+    } else {
+      throw new Error();
+    }
+  }
+  return 'Unable to determine the time of year!';
 };
