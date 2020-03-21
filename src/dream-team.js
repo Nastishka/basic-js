7640504f45113f6d87ca5458aa79ccea3c8b2b0a
Nastishka <anastasiya.matronkina.com>
2020-03-21 22:09:11 +0300

    feat: implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members && Array.isArray(members)) {
+    let firstLetters = [];
+    members.forEach(name => {
+      if (name && name.constructor === String) {
+        let trimmedName = name.trim();
+        if (trimmedName.length > 0) {
+          firstLetters.push(trimmedName[0].toUpperCase());
+        }
+      }
+    });
+    return firstLetters.sort().join('');
+  }
+  return false;
 };
