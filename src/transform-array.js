6ba479689f3d6f7124ad2f874958854cbe89f7a6
Nastishka <anastasiya.matronkina.com>
2020-03-22 11:45:59 +0300

    feat: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,33 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (Array.isArray(arr)) {
+        let transformedArr = [];
+        arr.forEach((item, index) => {
+            switch (item) {
+                case '--discard-next':
+                    break;
+                case '--discard-prev':
+                    if (transformedArr.length > 0) {
+                        transformedArr.pop();
+                    }
+                    break;
+                case '--double-next':
+                    if (arr.length > index + 1) {
+                        transformedArr.push(arr[index + 1]);
+                    }
+                    break;
+                case '--double-prev':
+                    let newIndex = index - 1;
+                    if (newIndex >= 0) {
+                        transformedArr.push(arr[newIndex]);
+                    }
+                    break;
+                default:
+                    if (index === 0 || arr[index - 1] != '--discard-next') {
+                        transformedArr.push(item);
+                    }
+            }
+
+        });
+        return transformedArr;
+    } else throw new Error();
 };
