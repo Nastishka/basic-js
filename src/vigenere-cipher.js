44d7595242ea8ac43ae1ca4e593dac4fd7de96ce
Nastishka <anastasiya.matronkina.com>
2020-03-22 23:42:05 +0300

    implement 'Vigenere Cipher' algorithm for encoding/decoding messages

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,60 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(isDirect = true) {
+        this.isDirect = isDirect;
+        this.alphabet = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    _generateKey(message, key) {
+        let keyInUpperCase = key.toUpperCase();
+        return Array.from(
+            { length: message.length },
+            (item, index) => keyInUpperCase[index % keyInUpperCase.length]
+        );
+    }
+
+    _transformMessage(message, key, transformFunc) {
+        if (message && key) {
+            let lettersToTransform = Array.from(message.toUpperCase()).filter(
+                letter => this.alphabet.indexOf(letter) >= 0
+            );
+            let generatedKey = this._generateKey(lettersToTransform, key);
+            let transformedLetters = Array.from(lettersToTransform,
+                (letterToTransform, index) => {
+                    let letterToTransformIndex = this.alphabet.indexOf(letterToTransform);
+                    let keyLetterIndex = this.alphabet.indexOf(generatedKey[index]);
+                    let transformedLetterIndex = transformFunc(letterToTransformIndex, keyLetterIndex);
+                    return this.alphabet[transformedLetterIndex];
+                }
+            );
+            let transformedMessage = Array.from(message.toUpperCase(),
+                (letter, index) => {
+                    if (this.alphabet.indexOf(letter) >= 0) {
+                        return transformedLetters.shift();
+                    } else {
+                        return letter;
+                    }
+                }
+            );
+            if (!this.isDirect) {
+                transformedMessage.reverse();
+            }
+            return transformedMessage.join('');
+        } else {
+            throw new Error();
+        }
+    }
+    encrypt(strToEncode, key) {
+        let encryptLetterFunc = (letterIndex, keyIndex) => {
+            return (letterIndex + keyIndex) % this.alphabet.length;
+        };
+        return this._transformMessage(strToEncode, key, encryptLetterFunc);
+    }
+
+    decrypt(strToDecode, key) {
+        let decryptLetterFunc = (letterIndex, keyIndex) => {
+            return (this.alphabet.length + letterIndex - keyIndex) % this.alphabet.length;
+        };
+        return this._transformMessage(strToDecode, key, decryptLetterFunc)
     }
 }
-
 module.exports = VigenereCipheringMachine;
